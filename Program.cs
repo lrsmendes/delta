76a51b7399fb68030daca70269423d894ddf6f09
Lucas Raziel <101843880+lrsmendes@users.noreply.github.com>
2022-03-17 21:26:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Por L.R. Mendes, (c) 2022
+            Console.WriteLine("(c)Raziel, 2022\n");
+
+            Double a, b, c, delta;
+            Console.WriteLine("Escreva os valores de A, B e C.\n");
+            Console.Write("A: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nB: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nC: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            delta = b * b - 4 * a * c;
+
+            Console.WriteLine("O seu delta é igual a: " + delta);
+
+            Console.Write("\nAperte uma tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
